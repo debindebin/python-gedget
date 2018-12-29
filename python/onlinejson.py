c3a908fa49c0a2fa250ff31b19f4411186bcc7c7
debindebin <whalewhalewhale@qq.com>
2018-12-29 10:11:23 +0800

    Update onlinejson.py

--- python/onlinejson.py
+++ python/onlinejson.py
@@ -2,7 +2,7 @@
 #__*__ coding: utf-8 __*__
 
 import redis
-r = redis.Redis(host='182.92.114.178', port=6381, db=0)
+r = redis.Redis(host='113.127.83.151', port=6385, db=0, password='password')
 home = r.get("HOME_PAGE_DEFINE")
 print home
 #home = home.replace("http://static.qiniu.yuenr.com/null", "http://static.qiniu.yuenr.com/home/1473685570349.png")
