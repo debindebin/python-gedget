a1819237e70f9cc20633d8908591cc8c4b6daaf9
debindebin <whalewhalewhale@qq.com>
2018-12-29 10:20:32 +0800

    Update HttpPost.py

--- python/HttpPost.py
+++ python/HttpPost.py
@@ -1,7 +1,7 @@
 import urllib 
 import urllib2
 
-url = "http://182.92.114.178:8801/yuenr/skill/getSkillDetail"
+url = "http://183.93.115.179:8802/yuenr/skill/getSkillDetail"
 parameters = {"accessToken":"id2rvbujPcZlvbFpvb8L6b6l64HN6cFr6bFf","deviceType":"1","id":"3738","versionName":"3.1.3","longitude":"0","latitude":"0"}
 
 data = urllib.urlencode(parameters)
