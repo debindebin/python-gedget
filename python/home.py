cf69a7a9a4b259b99f678cdd612851a827b5815a
debindebin <whalewhalewhale@qq.com>
2018-12-29 10:18:21 +0800

    Update home.py

--- python/home.py
+++ python/home.py
@@ -1,6 +1,6 @@
 import redis
 
-r= redis.Redis(host='182.92.114.178', port=6381, db=0)
+r= redis.Redis(host='113.127.83.151', port=6385, db=0, password='password')
 rr = redis.Redis(host='192.168.10.41', port=6379, db=0)
 
 #ss = rr.get("HOME_PAGE_DEFINE")
